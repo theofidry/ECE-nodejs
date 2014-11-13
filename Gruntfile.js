18a6bf5cd92b17072feb110ac05a862890b2b72c
Théo FIDRY <theo.fidry@gmail.com>
2014-11-13 11:52:06 +0100

    Installed Grunt, used Stylus with Grunt, cleaned code and fixed static files routing.

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+
+        stylus: {
+            dev: {
+                options: {
+                    compress: false,
+                    lineos: true
+                },
+                files: {
+                    'public/app.css': 'views/styl/app.styl'
+                }
+            },
+            prod: {
+                files: {
+                    'public/app.css': 'views/styl/app.styl'
+                }
+            }
+        },
+
+        watch: {
+            scripts: {
+                files: ['views/styl/**/*.styl'],
+                tasks: ['stylus:dev'],
+                options: {
+                    spawn: false    // for faster processing
+                }
+            }
+        }
+    });
+
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-stylus');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+    // Custom tasks
+    grunt.registerTask('css', ['stylus:dev']);
+    grunt.registerTask('css-prod', ['stylus:prod']);
+
+    grunt.registerTask('build', ['css']);
+    grunt.registerTask('publish', ['css-prod']);
+    grunt.registerTask('start', ['build', 'watch']);
+
+    // Default tasks
+    grunt.registerTask('default', ['start']);
+};
