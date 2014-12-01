7b01ed7573e28989c40d7fca1cd16b4489bb3fd9
Théo FIDRY <theo.fidry@gmail.com>
2014-12-01 11:03:48 +0100

    Added loader.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,13 +4,13 @@ $().ready(function () {
      * Do the post request in AJAX
      */
     $('#login').submit(function (event) {
+
         $.ajax({
             type:       "POST",
             url:        "/",
             cache:      false,
             beforeSend: function () {
-                //TODO: display connecting message
-                $('#login').val('Connecting...');
+                $('.loader').removeClass('hidden');
             }
         });
     });
