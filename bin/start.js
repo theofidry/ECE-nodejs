46641bf32002a1101c5cb9d90e1ca454f92d3db8
Théo FIDRY <theo.fidry@gmail.com>
2014-12-09 13:51:00 +0100

    Tryed to adapt it to OpenShift

--- bin/start.js
+++ bin/start.js
@@ -1,11 +1,10 @@
 // Load application module
 var app = require('../lib/app');
 
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    host = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+
 // Start server
-var server = app.listen(1337, 'localhost', function () {
-
-    var host = server.address().address,
-        port = server.address().port;
-
-    console.log('Server running at http://%s:%s', host, port);
+var server = app.listen(port, host, function () {
+    console.log('Server running at http://%s:%s', port, host);
 });
