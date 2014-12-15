bc265f199faaf8c8eacd3cada3041795274ce887
Théo FIDRY <theo.fidry@gmail.com>
2014-12-15 11:35:53 +0100

    Added socket.io

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,4 +14,11 @@ $().ready(function () {
             }
         });
     });
+
+    var socket = io('http://localhost');
+
+    socket.on('news', function (data) {
+        console.log(data);
+        socket.emit('my other event', { my: 'data' });
+    });
 });
